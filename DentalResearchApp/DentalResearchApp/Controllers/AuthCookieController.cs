ed31a43e663553a28694a460a72375b442cc240a
Aleksander Konnerup <Alek.k@hotmail.com>
2018-09-19 13:34:16 +0200

    - Lavet ændringer til routing for login og sådan lidt - Oprettet view til Signup

--- DentalResearchApp/DentalResearchApp/Controllers/AuthCookieController.cs
+++ DentalResearchApp/DentalResearchApp/Controllers/AuthCookieController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace DentalResearchApp.Controllers
 {
 
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class AuthCookieController : Controller
     {
--- DentalResearchApp/DentalResearchApp/Controllers/ValuesController.cs
+++ DentalResearchApp/DentalResearchApp/Controllers/ValuesController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DentalResearchApp.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
