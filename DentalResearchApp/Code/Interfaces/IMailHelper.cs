c4940b3a4f2cc40252edc94f7ccb91484c5e8d89
Simon <au532042@uni.au.dk>
2018-10-22 13:28:34 +0200

    Added mail sending class/methods

---
+++ DentalResearchApp/Code/Interfaces/IMailHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace DentalResearchApp.Code.Interfaces
+{
+    public interface IMailHelper
+    {
+        void SendMail(string receiverAdress, string messageSubject, string messageBody);
+    }
+}
--- DentalResearchApp/Controllers/LinkController.cs
+++ DentalResearchApp/Controllers/LinkController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using DentalResearchApp.Code.Impl;
 using DentalResearchApp.Models;
 using Microsoft.AspNetCore.Mvc;
