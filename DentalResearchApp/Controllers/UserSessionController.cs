7d0a7f47dd82d7683a7dd7d1729b36bbc0967343
CSJ <casperstenner@gmail.com>
2018-11-19 11:17:51 +0100

    Work in progress

---
+++ DentalResearchApp/Controllers/UserSessionController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DentalResearchApp.Models;
+using DentalResearchApp.Models.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DentalResearchApp.Controllers
+{
+    [Route("[controller]")]
+    public class UserSessionController : Controller
+    {
+
+        private readonly IContext _context;
+
+        public UserSessionController(IContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult UserSessions(int participantId, int studyId)
+        {
+            _context.ManagerFactory.CreateSessionManager();
+
+            var viewModel = new UserSessionsViewModel();
+
+
+            return View();
+        }
+
+        [HttpGet("UserSessionDetailsModal")]
+        public IActionResult UserSessionDetailsModal() //TODO:Send identifier
+        {
+
+
+
+            return View();
+        }
+    }
+}
